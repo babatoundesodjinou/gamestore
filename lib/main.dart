9e31f7cb32381c4819ea681a0065fa5a93cd6fe1
Amour SODJINOU <amoursodjinou@gmail.com>
2024-07-23 12:38:10 +0100

    feat: integration of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gamestore/pages/home/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  const MaterialApp(
       title: 'Game store',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomePage(),
     );
   }
 }
